f6696d4310f0bef854bf4bc62b85ff19ebbc3272
PoltixeTheDerg <34501060+PoltixeTheDerg@users.noreply.github.com>
2020-08-23 19:08:25 -0700

    Add mods and option to disable the large visual bursts
    Not sure if "Visual bursts" is the correct name, but this adds an ability to disable it because i was having problems reading with it in the way, and i'm sure others will too
    
    NF, HT, DC, DT, NC, and DC mods are added and functional

--- osu.Game.Rulesets.Diva/DivaRuleset.cs
+++ osu.Game.Rulesets.Diva/DivaRuleset.cs
@@ -9,6 +9,7 @@ using osu.Framework.Input.Bindings;
 using osu.Game.Beatmaps;
 using osu.Game.Configuration;
 using osu.Game.Rulesets.Difficulty;
+using osu.Game.Rulesets.Osu.Mods;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.UI;
 using osu.Game.Rulesets.Configuration;
@@ -17,6 +18,7 @@ using osu.Game.Rulesets.Diva.Mods;
 using osu.Game.Rulesets.Diva.UI;
 using osu.Game.Rulesets.Diva.Configuration;
 using osu.Game.Overlays.Settings;
+using osu.Game.Beatmaps.Legacy;
 
 namespace osu.Game.Rulesets.Diva
 {
@@ -39,12 +41,83 @@ namespace osu.Game.Rulesets.Diva
         public override IRulesetConfigManager CreateConfig(SettingsStore settings) => 
             new DivaRulesetConfigManager(settings, RulesetInfo);
 
+        public override IEnumerable<Mod> ConvertFromLegacyMods(LegacyMods mods)
+        {
+            if (mods.HasFlag(LegacyMods.Nightcore))
+                yield return new OsuModNightcore();
+            else if (mods.HasFlag(LegacyMods.DoubleTime))
+                yield return new OsuModDoubleTime();
+
+            if (mods.HasFlag(LegacyMods.Perfect))
+                yield return new OsuModPerfect();
+            else if (mods.HasFlag(LegacyMods.SuddenDeath))
+                yield return new OsuModSuddenDeath();
+
+            if (mods.HasFlag(LegacyMods.Autopilot))
+                yield return new OsuModAutopilot();
+
+            if (mods.HasFlag(LegacyMods.Cinema))
+                yield return new OsuModCinema();
+            else if (mods.HasFlag(LegacyMods.Autoplay))
+                yield return new OsuModAutoplay();
+
+            if (mods.HasFlag(LegacyMods.Easy))
+                yield return new OsuModEasy();
+
+            if (mods.HasFlag(LegacyMods.Flashlight))
+                yield return new OsuModFlashlight();
+
+            if (mods.HasFlag(LegacyMods.HalfTime))
+                yield return new OsuModHalfTime();
+
+            if (mods.HasFlag(LegacyMods.HardRock))
+                yield return new OsuModHardRock();
+
+            if (mods.HasFlag(LegacyMods.Hidden))
+                yield return new OsuModHidden();
+
+            if (mods.HasFlag(LegacyMods.NoFail))
+                yield return new OsuModNoFail();
+
+            if (mods.HasFlag(LegacyMods.Relax))
+                yield return new OsuModRelax();
+
+            if (mods.HasFlag(LegacyMods.SpunOut))
+                yield return new OsuModSpunOut();
+
+            if (mods.HasFlag(LegacyMods.Target))
+                yield return new OsuModTarget();
+
+            if (mods.HasFlag(LegacyMods.TouchDevice))
+                yield return new OsuModTouchDevice();
+        }
+
         public override IEnumerable<Mod> GetModsFor(ModType type)
         {
             switch (type)
             {
                 case ModType.Automation:
                     return new[] { new DivaModAutoplay() };
+
+                case ModType.DifficultyReduction:
+                    return new Mod[]
+                    {
+                        new OsuModNoFail(),
+                        new MultiMod(new OsuModHalfTime(), new OsuModDaycore()),
+                    };
+
+                case ModType.DifficultyIncrease:
+                    return new Mod[]
+                    {
+                        new OsuModSuddenDeath(),
+                        new MultiMod(new OsuModDoubleTime(), new OsuModNightcore()),
+                    };
+
+                case ModType.Conversion:
+                    return new Mod[]
+                    {
+                        new OsuModDifficultyAdjust(),
+                    };
 
                 default:
                     return new Mod[] { null };
