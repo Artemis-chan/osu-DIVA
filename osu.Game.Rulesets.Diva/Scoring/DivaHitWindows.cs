f64cb0e85a8584c4c7706f279783ebed84be4d21
TheSola10 <me@thesola.io>
2021-04-06 09:38:01 +0200

    Updated hit ranges (fixes #10)

--- osu.Game.Rulesets.Diva/Scoring/DivaHitWindows.cs
+++ osu.Game.Rulesets.Diva/Scoring/DivaHitWindows.cs
@@ -6,10 +6,10 @@ namespace osu.Game.Rulesets.Diva.Scoring
     {
         private static readonly DifficultyRange[] diva_ranges =
         {
-            new DifficultyRange(HitResult.Perfect, 80, 50, -20),
+            new DifficultyRange(HitResult.Perfect, 80, 50, 20),
             new DifficultyRange(HitResult.Great, 140, 100, 60),
             new DifficultyRange(HitResult.Meh, 200, 150, 100),
-            new DifficultyRange(HitResult.Miss, 300, 350, 400),
+            new DifficultyRange(HitResult.Miss, 400, 400, 400),
         };
 
         public override bool IsHitResultAllowed(HitResult result)
