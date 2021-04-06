9afe213ae4129d034a3f6afaf11e8ab21e76c00d
TheSola10 <me@thesola.io>
2021-04-06 22:17:17 +0200

    Drop custom hit offsets in favor of osu!mania's defaults

--- osu.Game.Rulesets.Diva/Scoring/DivaHitWindows.cs
+++ osu.Game.Rulesets.Diva/Scoring/DivaHitWindows.cs
@@ -5,21 +5,18 @@ namespace osu.Game.Rulesets.Diva.Scoring
 {
     public class DivaHitWindows : HitWindows
     {
-        protected override DifficultyRange[] GetRanges() => new DifficultyRange[]
-        {
-            new(HitResult.Great, 80, 50, 20),
-            new(HitResult.Ok, 140, 100, 60),
-            new(HitResult.Meh, 200, 150, 100),
-            new(HitResult.Miss, 400, 400, 400),
-        };
-
-        public override bool IsHitResultAllowed(HitResult result) =>
-            result switch {
-                HitResult.Great => true,
-                HitResult.Ok => true,
-                HitResult.Meh => true,
-                HitResult.Miss => true,
-                _ => false
-            };
+        public override bool IsHitResultAllowed(HitResult result) {
+            switch (result) 
+            {
+            case HitResult.Perfect:
+            case HitResult.Great:
+            case HitResult.Good:
+            case HitResult.Ok:
+            case HitResult.Meh:
+            case HitResult.Miss:
+                return true;
+            }
+            return false;
+        }
     }
 }
