0a0224adc727f9e80ce48de4c2acef52391829ee
Dao Heng Liu <daohengliu@gmail.com>
2022-10-18 12:29:02 +0100

    update DivaKeyMod, DivaModNoDoubles Description to LocalisableString

--- osu.Game.Rulesets.Diva/Mods/DivaModKey2.cs
+++ osu.Game.Rulesets.Diva/Mods/DivaModKey2.cs
@@ -1,6 +1,8 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using osu.Framework.Localisation;
+
 namespace osu.Game.Rulesets.Diva.Mods
 {
 	public class DivaModKey2 : DivaKeyMod
@@ -8,6 +10,6 @@ namespace osu.Game.Rulesets.Diva.Mods
 		public override int KeyCount => 2;
 		public override string Name => "Two Buttons";
 		public override string Acronym => "2B";
-		public override string Description => @"Play with two buttons.";
+		public override LocalisableString Description => @"Play with two buttons.";
 	}
 }
